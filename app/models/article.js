434227c5ef9185a81902ba6d880d1bb13d5d68e5
mooree1892 <edwardmoore@live.ca>
2017-07-08 13:50:45 -0400

    added list of items in the table of contents

--- app/models/article.js
+++ app/models/article.js
@@ -1,5 +1,7 @@
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-
+	postId: DS.attr('number'),
+	name: DS.attr('string'),
+	body: DS.attr('number')
 });
--- app/serializers/article.js
+++ app/serializers/article.js
@@ -1,4 +1,8 @@
 import DS from 'ember-data';
 
-export default DS.JSONAPISerializer.extend({
+export default DS.RESTSerializer.extend({
+	normalizeResponse(store, primaryModelClass, payload, id, requestType) {
+        payload = { articles : payload };
+        return this._super(store, primaryModelClass, payload, id, requestType);
+    }
 });
